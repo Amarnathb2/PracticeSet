cf9fb4ac12b7e7e6cc1481b0fd576e7bc70d5a6e
Amarnathb2 <jitendrak.samal@gmail.com>
2020-11-19 15:31:56 +0530

    prog exicuted...

--- src/com/projectcarv/praciceSet/project2021/Amarnath/Calculation.java
+++ src/com/projectcarv/praciceSet/project2021/Amarnath/Calculation.java
@@ -1,51 +1,81 @@
 import java.util.Scanner;
 class Calculation{
-	
-	public static void main(String args[]){
-	int choose,sum,sub,mul,div,num1=0,num2=0;
-	char operation;
-	Scanner sc = new Scanner(System.in);
-	
-	
-	System.out.println("Calculation Started...\n");
-	System.out.println("Choose which operation you want to do!!!");
-	System.out.println("1: Sum\n2: Sub\n3: Mult\n4: Div\n");
-	System.out.println("Choose your selection...");
-	choose=sc.nextInt();
-	System.out.println("Calculation Started...");
-	if(choose==1){
-		System.out.println("Enter first number: ");
-		num1=sc.nextInt();
-		System.out.println("Enter second number: ");
-		num2=sc.nextInt();
-		sum=num1+num2;
-		System.out.println("Result is :"+sum);
+  public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+   	int num1,num2,res;
+		
+
+
+	System.out.println("Which operation you want to do \n 1: Sum\n 2: Sub\n 3: Mult\n 4: Div");
+	int choice = sc.nextInt();
+      		    while (choice == 1) {
+		 	System.out.println("Enter first number: ");
+		  	num1=sc.nextInt();
+		    	System.out.println("Enter second number: ");
+		    	num2=sc.nextInt();
+		    	int sum=num1+num2;
+		    	System.out.println("Result is :"+sum);
+		    	System.out.println("Want more Operation y/n");
+		    	char op= sc.next().charAt(0);
+			if(op=='y'){
+				System.out.println("Which operation you want to do \n 1: Sum\n 2: Sub\n 3: Mult\n 4: Div");
+			        choice = sc.nextInt();
+			}
+			else{
+				break;
+			}
+			}
+		   while(choice == 2) {
+			System.out.println("Enter first number: ");
+			num1=sc.nextInt();
+			System.out.println("Enter second number: ");
+			num2=sc.nextInt();
+			int sub=num1-num2;
+			System.out.println("Result is :"+sub);
+			System.out.println("Want more Operation y/n");
+		    	char op= sc.next().charAt(0);
+			if(op=='y'){
+				System.out.println("Which operation you want to do \n 1: Sum\n 2: Sub\n 3: Mult\n 4: Div");
+			        choice = sc.nextInt();
+			}
+			else{
+				break;
+			}
 		}
-	else if(choose==2){
-		System.out.println("Enter first number: ");
-		num1=sc.nextInt();
-		System.out.println("Enter second number: ");
-		num2=sc.nextInt();
-		sub=num1-num2;
-		System.out.println("Result is :"+sub);
-		}
-	else if(choose==3){
-		System.out.println("Enter first number: ");
-		num1=sc.nextInt();
-		System.out.println("Enter second number: ");
-		num2=sc.nextInt();
-		mul=num1*num2;
-		System.out.println("Result is :"+mul);
-		}
-	else if(choose==4){
-		System.out.println("Enter first number: ");
-		num1=sc.nextInt();
-		System.out.println("Enter second number: ");
-		num2=sc.nextInt();
-		div=num1/num2;
-		System.out.println("Result is :"+div);
-		System.out.println("Want more operation y/n :");		
-		}
-	sc.close();
-}
+		while(choice==3){
+			System.out.println("Enter first number: ");
+			num1=sc.nextInt();
+			System.out.println("Enter second number: ");
+			num2=sc.nextInt();
+			int mul=num1*num2;
+			System.out.println("Result is :"+mul);
+			System.out.println("Want more Operation y/n");
+		    	char op= sc.next().charAt(0);
+			if(op=='y'){
+				System.out.println("Which operation you want to do \n 1: Sum\n 2: Sub\n 3: Mult\n 4: Div");
+			        choice = sc.nextInt();
+			}
+			else{
+				break;
+			}
+			}
+		while(choice==4){
+			System.out.println("Enter first number: ");
+			float n1=sc.nextFloat();
+			System.out.println("Enter second number: ");
+			float n2=sc.nextFloat();
+			Float div=n1/n2;
+			System.out.println("Result is :"+div);
+			System.out.println("Want more Operation y/n");
+		    	char op= sc.next().charAt(0);
+			if(op=='y'){
+				System.out.println("Which operation you want to do \n 1: Sum\n 2: Sub\n 3: Mult\n 4: Div");
+			        choice = sc.nextInt();
+			}
+			else{
+				break;
+			}
+   		 	}
+       
+      }
 }
